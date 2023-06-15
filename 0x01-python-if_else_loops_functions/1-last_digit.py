1c0456c144993a69d02fef92f47aa4b87c2364c9
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-06-15 13:43:55 +0300

    * **2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game**   * [2-print_alphabet.py](./2-print_alphabet.py): Python program that prints the alphabet   in lowercase, not followed by a new line.   * Using only one  and one loop.   * Without storing characters in variables or importing modules.

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = abs(number) % 10
+
+if number < 0:
+    last = -last
+    print("last digit of {} is {} and is ".format(number, last), end="")
+    if last == 0:
+        print("0")
+    elif last > 5:
+        print("greater than 5")
+    else:
+        print("less than 6 and not 0")
