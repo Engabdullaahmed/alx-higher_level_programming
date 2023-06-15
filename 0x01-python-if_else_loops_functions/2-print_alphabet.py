27d0154a38e65a6279f85bda1bacc3cb454fa587
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-06-15 14:25:55 +0300

    Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
    You can only use one print function with string format
    You can only use one loop in your code
    You are not allowed to store characters in a variable
    You are not allowed to import any module

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,5 +2,5 @@
 # Eng,Abdulla
 
 for letters in range(97, 123):
-    # print("{}".format(chr(letters)),end="")
+    # pri_n_t  ("{}".format(chr(letters)),end="")
     print(f"{chr(letters)}", end="")
