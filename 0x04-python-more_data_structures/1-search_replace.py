9fb1aad06f37c14d9c4e18f8f1b853ade5d05c28
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-06-16 23:43:52 +0300

    * **1. Search and replace**   * [1-search_replace.py](./1-search_replace.py): Python function that replaces all occurences   of an element by another in a new list.   * The parameter  is the initial list.   * The parameter  is the element to replace in the list.   * The parameter  is the new element.   * Without importing modules.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,6 @@
 # ENG,Abdullla
 
 
-def uniq_add(my_list=[]):
-    uniq_list = set(my_list)
-    num = 0
-
-    for i in uniq_list:
-        num += i
-
-    return (num)
+def search_replace(my_list, search, replace):
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
