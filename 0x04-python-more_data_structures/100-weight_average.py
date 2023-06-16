3fbd405131c8bc32406bdc8b48bb8203d9885b55
Eng Abdulla <ahmedmoustafaabdelwahed@gmail.com>
2023-06-17 00:28:50 +0300

    * **13. Weighted averageadd .*   * [100-weight_average.py](./100-weight_average.py): Python function that returns the   weighted average of all integers in a list of tuples.   * Tuple format: .   * If the list is empty - returns .   * Without importing modules.

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,5 +2,15 @@
 # Abdulla
 
 
-def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
